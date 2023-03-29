747ce5cfbf0a1a3559562221fe9cb340876f8e0b
Ilya Turkovsky <ilcheg@yandex.ru>
2023-03-29 14:28:46 +0300

    Add - Parsing Query String - Parsing Path

--- src/main/java/ru/netology/Request.java
+++ src/main/java/ru/netology/Request.java
@@ -1,16 +1,29 @@
 package ru.netology;
 
+import org.apache.http.NameValuePair;
+import org.apache.http.client.utils.URIBuilder;
+
 import java.io.BufferedReader;
+import java.net.URISyntaxException;
+import java.nio.charset.StandardCharsets;
+import java.util.List;
 
 public class Request {
     private final String method;
     private final String path;
     private final BufferedReader body;
+    private final List<NameValuePair> queryParams;
 
-    public Request(String method, String path, BufferedReader body) {
+    public Request(String method, String path, BufferedReader body) throws URISyntaxException {
         this.method = method;
-        this.path = path;
         this.body = body;
+        URIBuilder builder = new URIBuilder(path, StandardCharsets.UTF_8);
+        this.path = builder.getPath();
+        queryParams = builder.getQueryParams();
+    }
+
+    public List<NameValuePair> getQueryParams() {
+        return queryParams;
     }
 
     public String getMethod() {
